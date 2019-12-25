f247d1b58441403713bc0d16b5aca536890391e8
jayjeong <jayjeong1988@gmail.com>
2019-12-25 05:05:13 -0800

    do not commit when it's building.. otherwise you have to commit twice

---
+++ dist/precache-manifest.fcb72bab54e853f48e028250b4cb180d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584ba242cc7d3f58f1c4011e8da550db",
+    "url": "/car.ico"
+  },
+  {
+    "revision": "6d244a12d0c097598123",
+    "url": "/css/app.5f313169.css"
+  },
+  {
+    "revision": "8899b46d1b3520f97618",
+    "url": "/css/chunk-vendors.c580d68a.css"
+  },
+  {
+    "revision": "87ea693649333095a13ec0664459091c",
+    "url": "/img/JJ RIDE1.87ea6936.png"
+  },
+  {
+    "revision": "08b3d018113b0f23275fd5f5e98ac072",
+    "url": "/img/about.08b3d018.svg"
+  },
+  {
+    "revision": "240dc0f6e5ce3b15419b06ee18d6a667",
+    "url": "/img/admin.240dc0f6.svg"
+  },
+  {
+    "revision": "53cbf4988e9fc92616a6a9f77212c43c",
+    "url": "/img/cash.53cbf498.svg"
+  },
+  {
+    "revision": "8583169b0e25376b62792663f50b5f75",
+    "url": "/img/email.8583169b.svg"
+  },
+  {
+    "revision": "5d676a2a28e08588a749008b7f32a1b3",
+    "url": "/img/kakao-talk.5d676a2a.svg"
+  },
+  {
+    "revision": "5b69d510f32795be8218a5b3f5235f30",
+    "url": "/img/notification.5b69d510.svg"
+  },
+  {
+    "revision": "ee181b40a23050ea303e43064f73a506",
+    "url": "/img/payment.ee181b40.svg"
+  },
+  {
+    "revision": "ca93c6f2231e7d9d80ba60364d2f1c70",
+    "url": "/img/paypal.ca93c6f2.svg"
+  },
+  {
+    "revision": "0809b2c48ee6fc45b8b2cbd24db76375",
+    "url": "/img/smartphone.0809b2c4.svg"
+  },
+  {
+    "revision": "54b2c89b30a7651d4eccd0472e493d73",
+    "url": "/img/travel.54b2c89b.svg"
+  },
+  {
+    "revision": "b01a28f54b6b1e93bbf0eb5c3ddf1275",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d244a12d0c097598123",
+    "url": "/js/app.3d1477f4.js"
+  },
+  {
+    "revision": "8899b46d1b3520f97618",
+    "url": "/js/chunk-vendors.1fe11a06.js"
+  },
+  {
+    "revision": "b75c440b72f00815511e2c1dfaabe622",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ dist/sw.js
@@ -0,0 +1,3 @@
+importScripts("/precache-manifest.fcb72bab54e853f48e028250b4cb180d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+
