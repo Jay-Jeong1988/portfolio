728878813814be24853aa8dcc785cb7e6b008f48
jayjeong <jayjeong1988@gmail.com>
2019-12-24 19:04:36 -0800

    confirm localstorage in production works

--- src/store/index.js
+++ src/store/index.js
@@ -11,10 +11,8 @@ export default new Vuex.Store({
   mutations: {
     setAdmin(state) {
       var adminKey = localStorage.getItem("adminKey");
-      console.log(localStorage.getItem("adminKey"));
       if(adminKey && adminKey === process.env.VUE_APP_ADMINKEY) {
         state.isAdmin = true;
-        console.log(state.isAdmin)
       }
     }
   },
